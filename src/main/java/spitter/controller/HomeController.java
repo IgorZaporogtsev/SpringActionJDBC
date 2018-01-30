69f58d540d63798e78dc263f20ad8c9c3b4f7038
Igor Zaporogtsev <igorzaporogtsev@gmail.com>
2018-01-30 21:32:03 +0200

    Structure package for MVC Web project

---
+++ src/main/java/spitter/controller/HomeController.java
@@ -0,0 +1,29 @@
+package spitter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spitter.service.SpitterService;
+
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+        public static final int DEFAULT_SPITTLES_PER_PAGE = 25;
+
+        @Inject
+        public SpitterService spitterService;
+
+                                                        // Внедрить SpitterService
+        public HomeController(SpitterService spitterService) {
+        this.spitterService = spitterService;
+        }
+
+        @RequestMapping({"/","/home"})                                      // Обрабатывать запросы на получение главной страницы
+        public String showHomePage(Map<String, Object> model) {
+            model.put("spittles", spitterService.getRecentSpittles(
+                    DEFAULT_SPITTLES_PER_PAGE));                             //Добавить сообщения в модель
+            return "home";                                           // Вернуть имя представления
+        }
+
+}
---
+++ src/main/java/spitter/service/SpitterServiceImpl.java
@@ -0,0 +1,4 @@
+package spitter.service;
+
+public class SpitterServiceImpl {
+}
